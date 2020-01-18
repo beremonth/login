f4430fa1c0c9f3c6d0ac6f838b385fdd3afefd87
beremonth <beremonth@hotmail.com>
2020-01-18 11:54:19 -0600

    Creacion de modelo, para propiedades y metodos del usuario

---
+++ src/app/models/usuario.model.ts
@@ -0,0 +1,17 @@
+import { stringify } from 'querystring'
+
+// Nombre archivo: usuario.model.ts
+// Proposito: Almacenar las propiedades y métodos que puede tener el usuario
+// Autor: Stephen Arzeta
+// Fecha: 18/01/2020
+// Hora: 11:50 am
+
+// Ultima modificación:
+// Descripción:
+
+export class Usuario
+{
+    correo: string;
+    contrasena: string;
+    nombre: string;
+} // end class Usuario
